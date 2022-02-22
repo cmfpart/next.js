03f077c61fc3f6aee96523b0a8786a8b6840d568
cmfpart <100130009+cmfpart@users.noreply.github.com>
2022-02-22 19:15:48 +0700

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,10 @@
+export default function blog() {
+    return (
+      <>
+        <h1>CMF PART</h1>
+        <p>
+          Great information for your choice. Barang serius harga bercanda.
+        </p>
+      </>
+    );
+  }
